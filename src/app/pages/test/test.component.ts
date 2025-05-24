80a000843fd483025bf01aa4cff4932011df7504
emilien <emilien.sergeant@gmail.com>
2025-05-24 13:08:40 +0200

    rating

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,41 +1,69 @@
 import { AlertService } from './../../../services/alert.service';
 import { Component, inject } from '@angular/core';
-import { LoadingButtonComponent } from "../../../components/save-button/loading-button.component";
-import { CancelButtonComponent } from "../../../components/cancel-button/cancel-button.component";
-import { CardComponent } from "../../../components/card/card.component";
-import { ActionButtonsComponent } from "../../../components/action-buttons/action-buttons.component";
-import { BlockComponent } from "../../../components/block/block.component";
-import { CarouselComponent } from "../../../components/carousel/carousel.component";
-import { InputTextareaComponent } from "../../../components/input-textarea/input-textarea.component";
-import { SpinnerComponent } from "../../../components/spinner/spinner.component";
-import { InputTextComponent } from "../../../components/input-text/input-text.component";
-import { ToastComponent } from "../../../components/toast/toast.component";
-import { InputNumberComponent } from "../../../components/input-number/input-number.component";
-import { InputEmailComponent } from "../../../components/input-email/input-email.component";
-import { BreadCrumbComponent } from "../../../components/bread-crumb/bread-crumb.component";
-import { TreeNode, InfiniteTreeViewComponent } from '../../../components/treeview/treeview.component';
-import { SelectComponent } from "../../../components/select/select.component";
-import { InputDateComponent } from "../../../components/input-date/input-date.component";
-import { ChipSelectorComponent } from "../../chip-selector/chip-selector.component";
-import { CheckboxComponent } from "../../checkbox/checkbox.component";
-import { TimelineComponent } from "../../timeline/timeline.component";
+import { LoadingButtonComponent } from '../../../components/save-button/loading-button.component';
+import { CancelButtonComponent } from '../../../components/cancel-button/cancel-button.component';
+import { CardComponent } from '../../../components/card/card.component';
+import { ActionButtonsComponent } from '../../../components/action-buttons/action-buttons.component';
+import { BlockComponent } from '../../../components/block/block.component';
+import { CarouselComponent } from '../../../components/carousel/carousel.component';
+import { InputTextareaComponent } from '../../../components/input-textarea/input-textarea.component';
+import { SpinnerComponent } from '../../../components/spinner/spinner.component';
+import { InputTextComponent } from '../../../components/input-text/input-text.component';
+import { ToastComponent } from '../../../components/toast/toast.component';
+import { InputNumberComponent } from '../../../components/input-number/input-number.component';
+import { InputEmailComponent } from '../../../components/input-email/input-email.component';
+import { BreadCrumbComponent } from '../../../components/bread-crumb/bread-crumb.component';
+import {
+  TreeNode,
+  InfiniteTreeViewComponent,
+} from '../../../components/treeview/treeview.component';
+import { SelectComponent } from '../../../components/select/select.component';
+import { InputDateComponent } from '../../../components/input-date/input-date.component';
+import { ChipSelectorComponent } from '../../chip-selector/chip-selector.component';
+import { CheckboxComponent } from '../../checkbox/checkbox.component';
+import { TimelineComponent } from '../../timeline/timeline.component';
 import { Alert } from '../../../shared/model/alert';
 import { TextType } from '../../../shared/enum/text-enum-type';
-import { RangeComponent } from "../../range/range.component";
-import { FileUploadComponent } from "../../file-upload/file-upload.component";
+import { RangeComponent } from '../../range/range.component';
+import { FileUploadComponent } from '../../file-upload/file-upload.component';
+import { RatingComponent } from '../../rating/rating.component';
 
 @Component({
   selector: 'app-test',
-  imports: [LoadingButtonComponent, CancelButtonComponent, CardComponent, ActionButtonsComponent, BlockComponent, CarouselComponent, InputTextareaComponent,
-    SpinnerComponent, InputTextComponent, ToastComponent, InputNumberComponent, InputEmailComponent, BreadCrumbComponent, InfiniteTreeViewComponent, SelectComponent, InputDateComponent, ChipSelectorComponent, CheckboxComponent, TimelineComponent, RangeComponent, FileUploadComponent],
+  imports: [
+    LoadingButtonComponent,
+    CancelButtonComponent,
+    CardComponent,
+    ActionButtonsComponent,
+    BlockComponent,
+    CarouselComponent,
+    InputTextareaComponent,
+    SpinnerComponent,
+    InputTextComponent,
+    ToastComponent,
+    InputNumberComponent,
+    InputEmailComponent,
+    BreadCrumbComponent,
+    InfiniteTreeViewComponent,
+    SelectComponent,
+    InputDateComponent,
+    ChipSelectorComponent,
+    CheckboxComponent,
+    TimelineComponent,
+    RangeComponent,
+    FileUploadComponent,
+    RatingComponent,
+  ],
   standalone: true,
   templateUrl: './test.component.html',
-  styleUrl: './test.component.scss'
+  styleUrl: './test.component.scss',
 })
 export class TestComponent {
   alertService = inject(AlertService);
   startAction($event: Event) {
-    this.alertService.pushNewAlert(new Alert(TextType.success, 'Action started', 5000));
+    this.alertService.pushNewAlert(
+      new Alert(TextType.success, 'Action started', 5000)
+    );
   }
   treeData: TreeNode[] = [
     {
@@ -48,20 +76,17 @@ export class TestComponent {
           label: 'Child 1.1',
           children: [
             { id: '1.1.1', label: 'Grandchild 1.1.1', children: [] },
-            { id: '1.1.2', label: 'Grandchild 1.1.2', children: [] }
-          ]
+            { id: '1.1.2', label: 'Grandchild 1.1.2', children: [] },
+          ],
         },
-        { id: '1.2', label: 'Child 1.2', children: [] }
-      ]
+        { id: '1.2', label: 'Child 1.2', children: [] },
+      ],
     },
     {
       id: '2',
       label: 'Root Node 2',
-      children: [
-        { id: '2.1', label: 'Child 2.1', children: [] }
-      ]
-    }
+      children: [{ id: '2.1', label: 'Child 2.1', children: [] }],
+    },
   ];
   carouselItems: any;
-
 }
--- src/app/test-block/test-block.component.ts
+++ src/app/test-block/test-block.component.ts
@@ -1,25 +1,25 @@
 import { ModalService } from './../../services/modal.service';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ModalComponent } from "../modal/modal.component";
-import { ChipSelectorComponent } from "../chip-selector/chip-selector.component";
-import { SelectComponent } from "../../components/select/select.component";
-import { CheckboxComponent } from "../checkbox/checkbox.component";
-import { TimelineComponent } from "../timeline/timeline.component";
-import { RangeComponent } from "../range/range.component";
-import { FileUploadComponent } from "../file-upload/file-upload.component";
+import { ModalComponent } from '../modal/modal.component';
+import { ChipSelectorComponent } from '../chip-selector/chip-selector.component';
+import { SelectComponent } from '../../components/select/select.component';
+import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { TimelineComponent } from '../timeline/timeline.component';
+import { RangeComponent } from '../range/range.component';
+import { FileUploadComponent } from '../file-upload/file-upload.component';
+import { RatingComponent } from '../rating/rating.component';
 
 @Component({
   selector: 'app-test-block',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RatingComponent],
   templateUrl: './test-block.component.html',
-  styleUrl: './test-block.component.scss'
+  styleUrl: './test-block.component.scss',
 })
 export class TestBlockComponent {
-  constructor() {
-  }
+  constructor() {}
   form = new FormGroup({
-    mySelect: new FormControl('')
+    mySelect: new FormControl(''),
   });
 }
